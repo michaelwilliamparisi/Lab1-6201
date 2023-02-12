52b902e11975737efbd30bb12e62e32994fb4c57
Michael Parisi <michael_parisi@live.com>
2023-02-11 20:34:23 -0500

    commit

--- LAB1/scripts/services.js
+++ LAB1/scripts/services.js
@@ -1,3 +1,7 @@
 function changeBackGround(){
     document.body.style.backgroundImage = "url('images/trump.jpg')"
 }
+
+function divDisplayImg() {
+    document.getElementById("myDiv").style.backgroundImage = "url('cow.jpg')";
+  }
